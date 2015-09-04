4a3166555681cf8f114ea51d35a5fd50aff366fa
Sivan Patel <sivan.patel@googlemail.com>
2015-09-04 14:04:41 +0100

    Answered

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,6 +1,16 @@
 class PalindromeToTheDome
 
   def self.call(filename)
+    count = 0
+    File.readlines(filename).each do |line|
+      line.gsub!(/\W+/,'')
+      line.gsub!(/\s+/,'')
+      line.downcase!
+      if line == line.reverse
+        count += 1
+      end
+    end
+    count
   end
 
 end
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -5,9 +5,8 @@ describe PalindromeToTheDome do
   context 'when passed a filename' do
 
     it 'counts the number of palindromic lines in the file' do
-      expect(PalindromeToTheDome.call('textfile.md')).to equal(10)
+      expect(PalindromeToTheDome.call('textfile.md')).to equal(99)
     end
 
   end
 end
-
